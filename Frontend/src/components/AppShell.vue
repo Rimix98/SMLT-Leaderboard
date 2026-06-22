9368a7daeec449accf5257ff81e5fa7315d0b24e
Rimix98 <akademiaroman710@gmail.com>
2026-06-22 09:46:10 +0300

    Удалена надпись Roki из футера

--- Frontend/src/components/AppShell.vue
+++ Frontend/src/components/AppShell.vue
@@ -149,9 +149,6 @@ function closeInfoModal() {
   </header>
 
   <Teleport to="body">
-    <div class="roki-credit">Roki самый добый во всём SMLT</div>
-  </Teleport>
-  <Teleport to="body">
     <div class="modal-overlay" :class="{ active: hostModalOpen }" @mousedown="hostClose.onMousedown" @mouseup="hostClose.onMouseup">
       <div class="modal" @mousedown.stop @mouseup.stop>
         <div class="modal-header">
